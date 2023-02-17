3e7d9794bb8edcb6b85683fdf8c766983fa28fbb
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-02-17 13:18:54 +0530

    Added Alexa Notification API

---
+++ src/utils/alexa.ts
@@ -0,0 +1,26 @@
+export const sendNotificationToAlexa = async (
+  notification: string,
+  accessCode: string,
+  title = ""
+) => {
+  try {
+    const response = await fetch("api.notifymyecho.com/v1/NotifyMe", {
+      method: "POST",
+      body: JSON.stringify({
+        notification,
+        accessCode,
+        title,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (response.status !== 200) {
+      throw new Error(result?.message ?? "Error sending notification to alexa");
+    }
+
+    return true;
+  } catch (e: any) {
+    throw new Error(e?.message ?? "Error sending notification to alexa");
+  }
+};
